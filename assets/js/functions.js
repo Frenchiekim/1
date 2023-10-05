7ef516eee32babfd1a81b6108317d7757221788b
kim <kim.falardeau@gmail.com>
2023-10-05 17:21:52 -0400

    Projet final

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,15 +1,58 @@
-$( document ).ready(function() {
-	
-function nav(){
-	
-	$('.nav-toggle').click(function(){
-		
-		$('.nav').toggleClass('open');
-		
-	});
-	
+$(document).ready(function () {
+  function nav() {
+    $(".nav-toggle").click(function () {
+      $(".nav").toggleClass("open");
+    });
+  }
+
+  nav();
+  var map = L.map("map").setView([51.505, -0.09], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+});
+
+const slides = document.querySelectorAll('.slider');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+
+slides.forEach(function(slide, index) {
+  slide.style.left = `${index * 100}%`;
+})
+
+let counter = 0;
+
+
+
+nextBtn.addEventListener('click', function () {
+  counter++;
+  carousel();
+})
+
+prevBtn.addEventListener('click', function () {
+  counter++;
+  carousel();
+});
+
+function carousel() {
+	alert();
+  if (counter < slides.length -1) {
+    nextBtn.style.display = 'block';
+  } else {
+    nextBtn.style.display = 'none';
+  }
+
+  if (counter < 0) {
+    prevBtn.style.display = 'block';
+  } else {
+    prevBtn.style.display = 'none';
+  }
+
+  slides.forEach(function(slide) {
+	slide.style.transform = `translateX(-${counter * 100}%)`;
+  })
 }
 
-nav();
-
-});
+prevBtn.style.display = "none";
